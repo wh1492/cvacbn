cd81cec26f29b886d069e06ec3787888a45a3801
Leonelo Acaban <leoneloacaban@hotmail.com>
2022-06-09 11:28:53 -0300

    add: install icon pack

--- src/components/contacts/contacts.js
+++ src/components/contacts/contacts.js
@@ -1,27 +1,50 @@
 import React from "react";
+import { GrMail, GrSkype } from 'react-icons/gr';
+import { BsWhatsapp } from 'react-icons/bs';
+import { FaLinkedinIn } from 'react-icons/fa';
+import { HiLocationMarker } from "react-icons/hi";
+
+
 
 import './contacts.scss'
 
 const Contacts = (props) => {
-  const { mail, phone, linkedin, skype } = props.contacts
+  const { mail, phone, linkedin, skype, location } = props.contacts
 
   let linkdnTrim = linkedin.replace('https://', '')
   return (
-    <div className="py-4 wh-contact-block">
+    <div className=" wh-contact-block">
       <div className="container ">
         <ul className="row d-flex justify-content-between align-items-center">
-          <li className="col">
-            
-            <a href={"mailto:" + mail}>{mail}</a>
+          <li className="">
+            <a href={"mailto:" + mail}>
+              <GrMail />
+              {mail}
+            </a>
           </li>
-          <li className="col">
-            <a href={"tel:" + phone}>{phone}</a>
+          <li className="">
+            <a href={"tel:" + phone}>
+              <BsWhatsapp />
+              {phone}
+            </a>
           </li>
-          <li className="col">
-            <a href={linkedin} target="_blank">{linkdnTrim}</a>
+          <li className="">
+            <span >
+              <HiLocationMarker />
+              {location}
+            </span>
           </li>
-          <li className="col">
-            <a href={"skype:" + skype}>{skype}</a>
+          <li className="">
+            <a href={linkedin} target="_blank">
+              <FaLinkedinIn />
+              {linkdnTrim}
+            </a>
+          </li>
+          <li className="">
+            <a href={"skype:" + skype}>
+              <GrSkype />
+              {skype}
+            </a>
           </li>
         </ul>
       </div>
